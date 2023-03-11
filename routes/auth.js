b35268e088381019a4c9cec9cbe741b7dec49575
ceejayps <ceejayps1308@gmail.com>
2023-03-11 12:37:08 -0500

    Create auth.js

---
+++ routes/auth.js
@@ -0,0 +1,13 @@
+import express from 'express';
+
+import { signup, login } from '../controllers/auth.js';
+
+const router = express.Router();
+
+// POST /auth/signup
+router.post('/signup', signup);
+
+// POST /auth/login
+router.post('/login', login);
+
+export default router;
