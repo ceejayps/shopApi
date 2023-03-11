5043279804aaa6140a49dcf33bc31ded35f715d3
ceejayps <ceejayps1308@gmail.com>
2023-03-11 11:44:39 -0500

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,1 +1,15 @@
+import mongoose from 'mongoose';
+const db = async () => {
+  try {
+    const connection = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log();
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+};
 
+export default db;
